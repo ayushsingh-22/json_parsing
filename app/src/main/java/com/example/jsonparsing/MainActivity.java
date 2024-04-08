23bccc9c6f26e5d2d6ab35de67554cfb03b827c3
ayushsingh-22 <ayushkkumar2205@gmail.com>
2024-04-09 02:18:27 +0530

    Update

--- app/src/main/java/com/example/jsonparsing/MainActivity.java
+++ app/src/main/java/com/example/jsonparsing/MainActivity.java
@@ -5,17 +5,13 @@ import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.jsonparsing.R;
 
@@ -26,21 +22,14 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    String url = "https://jsonplaceholder.typicode.com/photos";
+    String url = "https://jsonplaceholder.typicode.com/users";
     ArrayList<String> arr = new ArrayList<>();
     ListView list; // Assuming you have a ListView with id "list" in your layout
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, ((Insets) systemBars).top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
         list = findViewById(R.id.list); // Initialize your ListView
 
         getApi();
@@ -49,17 +38,16 @@ public class MainActivity extends AppCompatActivity {
     private void getApi() {
         RequestQueue queue = Volley.newRequestQueue(this);
 
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
                     @Override
-                    public void onResponse(String response) {
+                    public void onResponse(JSONArray response) {
                         try {
-                            JSONArray jsonArray = new JSONArray(response);
-                            for (int i = 0; i < 50; i++) {
-                                JSONObject obj = jsonArray.getJSONObject(i);
-                                String title = obj.getString("title");
-                                String id = obj.getString("id");
-                                arr.add(id +" ---> "+title);
+                            for (int i = 0; i < response.length(); i++) {
+                                JSONObject obj = response.getJSONObject(i);
+                                String title = obj.getString("name");
+                                int id = obj.getInt("id");
+                                arr.add(id + ". " + title);
                             }
                             ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, arr);
                             list.setAdapter(adapter);
@@ -74,6 +62,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        queue.add(stringRequest);
+        queue.add(jsonArrayRequest);
     }
 }
